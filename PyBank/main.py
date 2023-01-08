85defc7111aa25d59a0423c574a270082892f544
angelanarag <119763672+ARNarag@users.noreply.github.com>
2023-01-07 21:07:28 -0800

    Update both PyBank and PyPoll main.py and added both output files

--- PyBank/main.py
+++ PyBank/main.py
@@ -36,11 +36,11 @@ pl_average = 0
 # Open the csv file
 with open(budget_file) as csvfile:
 
-    # CSV reader specifies delimiter and variable that holds contents
+    # Specify delimiter and variable that holds contents of the csv file
     budgetreader = csv.reader(csvfile, delimiter=',')
     # budgetreader = csv.DictReader(csvfile)
 
-    # Read the header row first (skip this step if there is now header)
+    # Read the header row first and skip to next so you don't include it in the count
     csv_header = next(budgetreader)
 
     # Read each row of data after the header
@@ -52,37 +52,40 @@ with open(budget_file) as csvfile:
         # Calculate the total profit loss
         total_pl = total_pl + int(row[1])
         
-        # calculate average change
-        # take the current month's p&l and subtract the previous month's p&l for change in pl. 
+        # Calculate average change
+        # Take the current month's p&l and subtract the previous month's p&l for p&l change 
         pl_change = int(row[1]) - prev_pl
         prev_pl = int(row[1])
-        # save each p&l change from month to month in a list 
+        # Save each p&l change from month to month in a list 
         pl_change_list = pl_change_list + [pl_change]
         month_of_change = [month_of_change] + [row[0]]
 
-        # find the greatest increase in p&l change and the month
+        # Find the greatest increase in p&l change and the corresponding month
         if pl_change > greatest_increase[1]:
+            # Places the greatest increase p&l changes in the second position
             greatest_increase[1] = pl_change
+            # Places the corresponding month in the first position
             greatest_increase[0] = row[0]
 
-        # find the greatest decrease in p&l change and the month
+        # Find the greatest decrease in p&l change and the corresponding month
         if pl_change < greatest_decrease[1]:
             greatest_decrease[1] = pl_change
             greatest_decrease[0] = row[0]
+        
+        # Once script has gone through all the rows, you will have a list of all p&l changes
+        # As well as the greatest increase and greatest decrease in p&l change 
 
-
-    # average is calculated by adding each p&l change and then dividing by the count
-    # take the p&l change list gathered above for the formula
-    pl_average = sum(pl_change_list)/len(pl_change_list)
+    # Calculate average p&l by summing all the p&l changes and dividing by the count
+    pl_average = round(sum(pl_change_list)/len(pl_change_list), 2)
 
 # print results to the terminal
 print("Financial Analysis")
 print("------------------------------------------------------")
 print(f'Total Months: {total_months}')
-print(f'Total: {total_pl}')
-print(f'Average Change: {pl_average}')
-print(f'Greatest Increase in Profits: {greatest_increase}')
-print(f'Greatest Decrease in Profits: {greatest_decrease}')
+print(f'Total: ${total_pl}')
+print(f'Average Change: ${pl_average}')
+print(f'Greatest Increase in Profits: {greatest_increase[0]}, (${greatest_increase[1]})')
+print(f'Greatest Decrease in Profits: {greatest_decrease[0]}, (${greatest_decrease[1]})')
         
 
 
@@ -91,8 +94,8 @@ with open(budget_output, 'w') as file:
     file.write("Financial Analysis\n")
     file.write("--------------------------------------------------\n")
     file.write(f'Total Months: {total_months}\n')
-    file.write(f'Total: {total_pl}\n')
-    file.write(f'Average Change: {pl_average}\n')
-    file.write(f'Greatest Increase in Profits: {greatest_increase}\n')
-    file.write(f'Greatest Decrease in Profits: {greatest_decrease}\n')
+    file.write(f'Total: ${total_pl}\n')
+    file.write(f'Average Change: ${pl_average}\n')
+    file.write(f'Greatest Increase in Profits: {greatest_increase[0]}, (${greatest_increase[1]})\n')
+    file.write(f'Greatest Decrease in Profits: {greatest_decrease[0]}, (${greatest_decrease[1]})\n')
     
